442cd126f284ea1f13b704c01e4cc345cbf53e8b
JAMES <JAMES@LAPTOP-6JUF463P>
2022-09-14 09:38:01 -0500

    Agregar archivos de proyecto.

---
+++ MyFirstAppSol/MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+String message = String.Empty;
+Console.WriteLine("Proporciona un mesanje por favor: ");
+message = Console.ReadLine();
+Console.WriteLine("Tu cadena es: " + message);
+Console.Read();
+
---
+++ PrimerEjercicioSol/PrimerEjercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("escribe el precio del producto: ");
+double valor = Convert.ToDouble(Console.ReadLine ());
+
+Console.WriteLine("escribe tu forma de pago, efectivo o tarjeta: ");
+String precio = (Console.ReadLine()).ToLower();
+
+
+if (precio.Equals("tarjeta")){
+
+    Console.WriteLine("Introduce el numero de tarjeta: ");
+    int numero_tarjeta = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("el producto con precio " + precio + "se ha pagado " + "con el numero de cuenta " + numero_tarjeta );
+
+}
+else if (precio.Equals("efectivo"))
+{
+    Console.WriteLine("El producto con precio " + precio + "se ha pagado");
+}
+else
+{
+    Console.WriteLine("El valor ingresado no es correcto");
+}
+Console.ReadLine();
+
+
+
+
+
+
+
